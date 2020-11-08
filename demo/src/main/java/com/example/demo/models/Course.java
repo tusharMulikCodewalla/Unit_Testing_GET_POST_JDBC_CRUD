61ee5a0f594f4c5d945f5666f86abf2738db5659
codewalla <codewalla@codewalla-HP-15-Notebook-PC>
2020-11-08 17:56:27 +0530

    First Commit

---
+++ demo/src/main/java/com/example/demo/models/Course.java
@@ -0,0 +1,106 @@
+package com.example.demo.models;
+
+import java.util.List;
+
+public class Course {
+	
+	private String id;
+	private String name;
+	private String description;
+	private List<String> steps;
+	
+	public Course() {
+		super();
+	}
+	
+	public Course(String id, String name, String description, List<String> steps) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.steps = steps;
+	}
+	
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	public List<String> getSteps() {
+		return steps;
+	}
+	
+	public void setSteps(List<String> steps) {
+		this.steps = steps;
+	}
+	
+	@Override
+	public String toString() {
+		return "Course [id=" + id + ", name=" + name + ", description=" + description + ", steps=" + steps + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((description == null) ? 0 : description.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((steps == null) ? 0 : steps.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Course other = (Course) obj;
+		if (description == null) {
+			if (other.description != null)
+				return false;
+		} else if (!description.equals(other.description))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (steps == null) {
+			if (other.steps != null)
+				return false;
+		} else if (!steps.equals(other.steps))
+			return false;
+		return true;
+	}
+	
+	
+
+}
